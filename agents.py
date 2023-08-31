7b4127751ec9c17181631f53cf081d972f624ffb
Arvind Shyam <a.shyam.2313@gmail.com>
2023-08-31 16:12:17 -0400

    added agents and worked on if-statements

--- agents.py
+++ agents.py
@@ -4,8 +4,25 @@ from structure import Structure
 '''conv different for all agents.'''
 ''' I'll form more agents as time goes on... here are three to begin with'''
 
-class ShortSimpleAgent():
+class Agent():
+    # boilerplate class
+    def __init__(self) -> None:
+        self.structures = []
+        self.s_structure = []        
+        self.params = {}
+        # The rest of the stuff depends on the agent.
+    
+    def form_structures(self):
+        conv = {"I": "intro", "V":"verse", "C": "chorus", "O":"outro", "B":"bridge"
+                , "S": "solo", "IL": "interlude"}
+        structs = [Structure(name=conv[x], params=self.params) for x in self.s_structure]
+        return structs
+
+
+class ShortSimple(Agent):
+    ''' Short length, simple structures, no changes'''
     def __init__(self, params) -> None:
+        super().__init__()
         self.structures = [
             ["I", "V", "C", "V", "C", "O"],
             ["I", "V", "C", "V", "O"],
@@ -14,45 +31,75 @@ class ShortSimpleAgent():
         self.params = params
         self.structs = self.form_structures()
 
-    def form_structures(self):
-        conv = {"I": "intro", "V":"verse", "C": "chorus", "O":"outro"}
-        structs = [Structure(name=conv[x], params=self.params) for x in self.s_structure]
-        return structs
 
-
-class SimpleAgent():
+class ShortComplex(Agent):
+    ''' Short length, more complex structures, no changes'''
     def __init__(self, params) -> None:
-        structures = [
+        super().__init__()
+        self.structures = [
             ["I", "V", "C", "V", "C", "O"],
             ["I", "V", "V", "C", "B", "C", "O"],
-            ["I", "V", "C", "V", "C", "V", "O"],
-            ["I", "V", "C", "V", "V", "C", "O"],
+            ["I", "V", "C", "V", "S", "V", "O"],
+            ["I", "V", "C", "V", "B", "C", "O"],
         ]
-        self.song_structure = random.choice(structures)
+        self.s_structure = random.choice(self.structures)
         self.params = params
         self.structs = self.form_structures()
 
-    def form_structures(self):
-        conv = {"I": "intro", "V":"verse", "C": "chorus", "O":"outro", "B":"bridge"}
-        structs = [Structure(name=conv[x], params=self.params) for x in self.s_structure]
-        return structs
 
-
-class LongComplex():
+class MediumSimplistic(Agent):
+    '''Medium length, simple structures, few changes'''
     def __init__(self, params) -> None:
-        structures = [
-            ["I", "V", "C", "B", "V", "S", "C", "O"],
-            ["I", "V", "C", "V", "S", "B", "S", "V", "O"],
-            ["I", "V", "V", "C", "B", "V", "S", "V", "C", "O"],
-            ["I", "V", "S", "C", "V", "B", "V", "C", "O"],
-            ["I", "V", "C", "V", "B", "S", "V", "B", "C", "O"]
+        super().__init__()
+        self.structures = [
+            ["I", "V", "C", "V", "C", "V", "C", "O"],
+            ["I", "V", "V", "C", "B", "V", "V" "C", "O"],
+            ["I", "V", "C", "V", "IL", "C", "V", "O"],
+            ["I", "V", "C", "V", "B", "V", "C", "O"],
         ]
-        self.song_structure = random.choice(structures)
+        self.s_structure = random.choice(self.structures)
+        self.params = params
         self.structs = self.form_structures()
 
-    def form_structures(self):
-        conv = {"I": "intro", "V":"verse", "C": "chorus", "O":"outro", "B":"bridge"
-                , "S": "solo"}
-        structs = [Structure(name=conv[x], params=self.params) for x in self.s_structure]
-        return structs
 
+class MediumComplex(Agent):
+    '''Medium length, complex structures, some changes'''
+    def __init__(self, params) -> None:
+        super().__init__()
+        self.structures = [
+            ["I", "V", "C", "B", "V", "C", "S", "C", "O"],
+            ["I", "V", "C", "IL", "V", "C", "S" "C", "O"],
+            ["I", "V", "V", "C", "IL", "V", "S", "C", "O"],
+            ["I", "V", "C", "B", "V", "S", "C", "V", "C" "O"],
+        ]
+        self.s_structure = random.choice(self.structures)
+        self.params = params
+        self.structs = self.form_structures()
+
+
+class LongSimplistic(Agent):
+    '''Long length, somewhat simplistic structures, some changes'''
+    def __init__(self, params) -> None:
+        super().__init__()
+        self.structures = [
+            ["I", "V", "C", "V", "C", "B", "V", "C", "V", "C", "O"],
+            ["I", "V", "C", "B", "V", "C", "V", "S", "V", "C", "O"],
+            ["I", "V", "V", "C", "V", "B", "V", "C", "S", "C", "O"]
+        ]
+        self.s_structure = random.choice(self.structures)
+        self.params = params
+        self.structs = self.form_structures()
+
+
+class LongComplex(Agent):
+    ''' Long length, complex structures, multiple changes'''
+    def __init__(self, params) -> None:
+        super().__init__()
+        self.structures = [
+            ["I", "V", "C", "V", "B", "S", "V", "C", "IL", "V", "C", "O"],
+            ["I", "V", "C", "V", "C", "S", "V", "B", "V", "S", "C", "O"],
+            ["I", "V", "V", "C", "B", "V", "C", "S", "C", "IL", "C", "O"]
+        ]
+        self.s_structure = random.choice(self.structures)
+        self.params = params
+        self.structs = self.form_structures()
--- preferences.py
+++ preferences.py
@@ -1,37 +1,24 @@
 from agents import * 
 
-print("------------- PREFERENCES -------------")
 
-song_complexity = int(input("Song Structure Complexity  "))
-length = input("Length  ")
-chord_complexity = int(input("Chord Complexity  "))
-variety = int(input("Variety  "))
-sound = int(input("Consonance & Dissonance  "))
-accidentals = int(input("Accidentals  "))
-common_chords = input("Common Chords  ").split(" ")
-range = input("Highest Note  "), input("Lowest Note  ")
-modulaton = input("Modulation  ").split()
-key = input("Key  ")
-tempo = int(input("Tempo  "))
+print("------------- PREFERENCES -------------")
+print("AI will try its best to adhere to preferences, but it may not.")
+print("Conflicting preferences may not all be followed through by AI.")
+print("You'll be informed if the AI couldn't follow through with your prefernces.\n\n")
 
 params = {
-    "song_complexity": song_complexity,
-    "length": length,
-    "chord_complexity": chord_complexity,
-    "variety": variety,
-    "sound": sound,
-    "accidentals": accidentals,
-    "common_chords": common_chords,
-    "range": range,
-    "modulation": modulaton,
-    "key": key,
-    "tempo": tempo,
+    "s_complexity": int(input("Song Structure Complexity:  ")),
+    "length": int(input("Length:  ")),
+    "chord_complexity": int(input("Chord Complexity:  ")),
+    "variety": int(input("Variety:  ")),
+    "sound": int(input("Consonance/Dissonance:  ")),
+    "accidentals": int(input("Accidentals (range of 1-5):  ")),
+    "common_chords": input("List Common Chords:  ").split(),
+    "range": (input("Highest Note:  "), input("Lowest Note  ")),
+    "modulation": input("List key changes:  ").split(),
+    "key": input("Enter key:  "),
+    "tempo_range": (int(input("Minimum tempo")), int(input("Maximum Tempo"))),
 }
 
-# Song complexity:
-if song_complexity < 3 and length == "short":
-    agent = ShortSimpleAgent(params=2)
-elif song_complexity < 3 and length == "long":
-    agent = SimpleAgent(params)
-else:
-    agent = LongComplex(params)
+def assign_agent(params):
+    pass
