9f44bd7110be5e6413934b27270d1e1c21eaca89
Arvind Shyam <a.shyam.2313@gmail.com>
2023-09-12 17:44:11 -0400

    added structure boilerplate

--- preferences.py
+++ preferences.py
@@ -17,8 +17,37 @@ params = {
     "range": (input("Highest Note:  "), input("Lowest Note  ")),
     "modulation": input("List key changes:  ").split(),
     "key": input("Enter key:  "),
-    "tempo_range": (int(input("Minimum tempo")), int(input("Maximum Tempo"))),
+    "tempo_range": (int(input("Min tempo  ")), int(input("Max Tempo  "))),
 }
 
 def assign_agent(params):
-    pass
+    if params["length"] == 1 or params["length"] == 2:
+        if params["s_complexity"] <= 3:
+            agent = ShortSimple(params)
+        else:
+            agent = ShortComplex(params)
+    elif params["length"] == 3:
+        if params["s_complexity"] < 3:
+            # this time < 3, but <= 3
+            agent = MediumSimplistic(params)
+        else:
+            agent = MediumComplex(params) 
+    else:
+        if params["s_complexity"] < 3:
+            # this time < 3, but <= 3
+            agent = LongSimplistic(params)
+        else:
+            agent = LongComplex(params) 
+    
+    return agent
+
+agent = assign_agent(params)
+print(agent.__class__.__name__)
+print(agent.structs)
+a = agent.structs[0]
+print("SETTINGS FOR INTRO\n")
+print(f"Length of structure components: {a.len}")
+print(f"Number of UNIQUE components: {a.num_components}")
+print(f"Number of accidentals (not taken to account): {a.num_acc}")
+print(a.cs)
+print()
--- structure.py
+++ structure.py
@@ -4,15 +4,61 @@ class Structure():
     def __init__(self, name, params):
         self.name = name
         self.params = params
-        
-        # Determine the segment arrangement
-        # if params["song_complexity"] == 1 and params["length"] == "short":
-        #     pass
 
+        self.len, self.num_components, self.num_acc = self.gen_params()
+        self.cs = self.gen_components()
         self.generate()
 
     def __repr__(self):
         return f"Song Structure: {self.name}"
+    
+    def gen_components(self):
+        comp_structure = (10 * list("ABCDEFGHIJKL")[:self.num_components])[:self.len]
+        random.shuffle(comp_structure)
+        return comp_structure
+    
+    def gen_params(self):
+        ''' Generates the component structure for the song structure'''
+
+        # decide on song length
+        if self.params["length"] == 1:
+            len = 5
+        elif self.params["length"] == 2:
+            len = 7
+        elif self.params["length"] < 5:
+            len = 9
+        else:
+            len = random.randint(10, 11)
+        
+        # decide on variety
+        if self.params["variety"] == 1:
+            num_components = min(2, len)
+        elif self.params["variety"] == 2:
+            num_components = min(3, len)
+        elif self.params["variety"] == 3:
+            num_components = min(4 if len <= 7 else 6, len)
+        elif self.params["variety"] == 4:
+            num_components = min(7 if len < 10 else random.randint(8, 9), len)
+        else:
+            num_components = len
+        
+        # decide on accidentals
+        num_repeats = len - num_components
+        if self.params["accidentals"] == 1:
+            num_acc = min(num_repeats, 1)
+        elif self.params["accidentals"] < 4:
+            num_acc = min(num_repeats, 3)
+        elif self.params["accidentals"] == 4:
+            num_acc = min(num_repeats, 4)
+        else:
+            num_acc = min(num_repeats, random.randint(5,6))
+
+        # Parameters:
+        # len = length of song
+        # num_components = number of different components in song
+        # num_acc = number of accidentals for SAME components only
+        return len, num_components, num_acc
+
             
     def generate(self):
         ''' Generates the chord sequence using the NN. '''
